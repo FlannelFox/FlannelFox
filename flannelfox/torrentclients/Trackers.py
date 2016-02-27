d6ce7aa4178bea5ec3eff07c796dd5d4875ac55d
FlannelFox <FlannelFox@OpenMailbox.org>
2016-02-26 21:59:34 -0500

    documentation update

--- flannelfox/torrentclients/Trackers.py
+++ flannelfox/torrentclients/Trackers.py
@@ -7,6 +7,18 @@
 # -*- coding: utf-8 -*-
 
 class Responses(object):
+    '''
+    Tracker responses we can get and if we should consider them for torernt
+        removal or not. It is important to not that these are partial matches;
+        this means if the phrase is found anywhere in the tracker reply it
+        will be labeled as such.
+
+
+    Remove is a list of messages that should cause the torrent to be removed
+
+    OK is a list of messages that are ok and should not result in the torrent
+        being removed. (Not used as of yet, but could be in the future)
+    '''
     Remove = [
         'unregistered torrent',
         'Unregistered torrent',
