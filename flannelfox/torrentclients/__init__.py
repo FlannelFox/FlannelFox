3bd533fbb789b57ac29c255f656bafe3debd4ead
FlannelFox <FlannelFox@OpenMailbox.org>
2016-02-26 22:01:43 -0500

    documentation update for torrentclients/__init__.py

---
+++ flannelfox/torrentclients/__init__.py
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------
+# Name:        torrentclients
+# Purpose:     This should be a generic torrent client that other clients
+#              can and should extend from in order to keep certain things sane.  
+#
+# TODO: 
+#              Build a base class that other torrent clients can use.
+#-------------------------------------------------------------------------------
+# -*- coding: utf-8 -*-
