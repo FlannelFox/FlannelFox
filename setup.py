2cb0b46863ff6115738d1f4c3670d510d40289c4
FlannelFox <FlannelFox@OpenMailbox.org>
2016-03-10 16:26:42 -0500

    Updated Version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os.path as osPath
 import sys
 from setuptools import setup, find_packages
 setup(name = 'flannelfox',
-      version = '0.2.0',
+      version = '1.1.2',
       packages = find_packages(),
       package_data={'flannelfox': ['flannelfox/data/*']},
       data_files = [
